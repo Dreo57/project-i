96c31abfc12a5ea94135b3c8bf18a1b24f7f15e4
Ope Dare <dreoly57@gmail.com>
2022-11-05 12:25:31 -0600

    updated s3

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "sg" {
 
 module "s3" {
     # source = "../personal-project/sg"
-    source = "git::https://github.com/Dreo57/Project-1.git//S3"
-    
+    source = "git::https://github.com/Dreo57/Project-1.git//s3"
+
 }
 
 
