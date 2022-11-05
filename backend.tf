128a8ae42a013b51662acd9d3d34d76c13fd9a25
Ope Dare <dreoly57@gmail.com>
2022-11-05 10:25:56 -0600

    created backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# Below is Backend block
+terraform {
+  backend "s3" {
+    bucket = "airbnb-statefile-2211"
+    key = "project-i.tfstate"
+    region = "us-east-1"
+    profile = "default"
+    dynamodb_table = "airbnb-statefile-lock"
+  }
+}
