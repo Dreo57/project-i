ba853a6c359bb3b218dc60c9ac6cb53a6f5d0437
Ope Dare <dreoly57@gmail.com>
2022-11-05 11:08:22 -0600

    backend ammended

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "airbnb-statefile-2211"
-    key = "project-i.tfstate"
+    key = "project-i1.tfstate"
     region = "us-east-1"
     profile = "default"
     dynamodb_table = "airbnb-statefile-lock"
