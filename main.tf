77648a80638a315022ad15eb575624a97b13132b
Ope Dare <dreoly57@gmail.com>
2022-11-05 21:22:46 -0600

    endpoint code added

--- main.tf
+++ main.tf
@@ -38,7 +38,6 @@ module "endpoint" {
     vpc_id = module.vpc.vpc_id
 }
 
-
 # module "rds" {
 #     # source = "../personal-project/rds"
 #     source = "git::https://github.com/Dreo57/Project-1.git//rds"
