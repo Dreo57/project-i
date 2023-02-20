b925a64b95b68bd49544d6afe5642f7da014ede9
Dreo57 <98928149+Dreo57@users.noreply.github.com>
2023-02-19 22:37:04 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    shared_credentials_file = "C:\Users\opeda\.aws\credentials"
+    shared_credentials_file = "/Users/opeda/.aws/credentials"
     profile = "default"
 }
